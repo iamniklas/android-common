f3612b1e1b20384ea62cb55d939127c56636cef6
iamniklas <niklas.englmeier@web.de>
2022-02-23 22:43:43 +0100

    Update LoginActivity.kt

--- androidcommon/src/main/java/de/niklasenglmeier/androidcommon/LoginActivity.kt
+++ androidcommon/src/main/java/de/niklasenglmeier/androidcommon/LoginActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.text.InputType
 import android.view.View
+import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.auth.api.signin.GoogleSignIn
